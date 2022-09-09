8d280d939bf1bed18dedb87cad1829347152d089
Nikita <trabelsi@inbox.ru>
2022-09-09 23:54:19 +0300

    Асинхронное взаимодействие [#458497]

--- src/main/java/ru/plifis/job4j_client_passport/client/PassportClient.java
+++ src/main/java/ru/plifis/job4j_client_passport/client/PassportClient.java
@@ -5,6 +5,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import ru.plifis.job4j_client_passport.model.PassportDto;
@@ -67,5 +68,10 @@ public class PassportClient {
                 String.format("%s/find-replaceable", url),
                 HttpMethod.GET, null, new ParameterizedTypeReference<List<PassportDto>>() {}
         ).getBody();
+    }
+
+    @Scheduled(fixedDelay = 2000)
+    public void checkUnavailablePassports() {
+        getUnavailablePassports();
     }
 }
---
+++ src/main/java/ru/plifis/job4j_client_passport/config/JobsSchedulerConfig.java
@@ -0,0 +1,9 @@
+package ru.plifis.job4j_client_passport.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class JobsSchedulerConfig {
+}
